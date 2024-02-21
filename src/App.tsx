0ee175fc1ead015d5cd3367128da15dd4487ec51
Justin <yenyao@hotmail.co.uk>
2024-02-21 09:48:58 +0000

    Allow calculator to recall previous equations

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,26 @@
 import { useState } from "react";
 import "./App.css";
 import Numpad from "./Components/Numpad/Numpad";
-import FormatDisplay from "./Utils/Util";
+import FormatDisplay from "./Utils/Input";
+import { OperationSymbol } from "./Constants/Operations";
 
 function App() {
   const [display, setDisplay] = useState({
     equation: "0",
     operation: "",
+    previousEquations: ["Equation 1", "Equation 2", "Equation 3"] as string[],
+    index: -1,
   });
 
-  const newDisplay = (newDisplayMessage: string) => {
-    setDisplay(FormatDisplay(display, newDisplayMessage));
+  const newDisplay = (recentInput: string) => {
+    setDisplay(FormatDisplay(display, recentInput));
   };
 
   return (
     <>
       <div className="result-display">{display.equation}</div>
+      <button onClick={() => newDisplay(OperationSymbol.Up)}>{"↑"}</button>
+      <button onClick={() => newDisplay(OperationSymbol.Down)}>{"↓"}</button>
       <Numpad CalcInput={newDisplay} />
     </>
   );
---
+++ src/Constants/Operations.tsx
@@ -0,0 +1,13 @@
+export enum OperationSymbol {
+  Down = "Down",
+  Up = "Up",
+  Backspace = "←",
+  Clear = "C",
+  Equal = "=",
+  Multiply = "x",
+  Divide = "/",
+  Percentage = "%",
+  Add = "+",
+  Subtract = "-",
+  Root = "√",
+}
